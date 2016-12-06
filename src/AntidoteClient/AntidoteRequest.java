c64546ec09ee47d02f3090a319386e1bb732678a
Luc Francois <francois.luc93@gmail.com>
2016-12-06 23:21:22 +0100

    Upload new file

---
+++ src/AntidoteClient/AntidoteRequest.java
@@ -0,0 +1,24 @@
+package AntidoteClient;
+import com.google.protobuf.AbstractMessage;
+
+public class AntidoteRequest {
+    private int code;
+    private AbstractMessage message;
+
+    public AntidoteRequest(int c, AbstractMessage m) {
+        code = c;
+        message = m;
+    }
+
+    public int getLength() {
+        return message.getSerializedSize() + 1;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public AbstractMessage getMessage() {
+        return message;
+    }
+}
