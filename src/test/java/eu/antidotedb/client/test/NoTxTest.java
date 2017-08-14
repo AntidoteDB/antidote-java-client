00b79eff5c7ff6eb24205915c180cc2714d82b60
Peter Zeller <p_zeller@cs.uni-kl.de>
2017-08-14 15:59:04 +0200

    testcase for negative increments

--- src/test/java/eu/antidotedb/client/test/NoTxTest.java
+++ src/test/java/eu/antidotedb/client/test/NoTxTest.java
@@ -1,5 +1,6 @@
 package eu.antidotedb.client.test;
 
+import eu.antidotedb.client.CounterRef;
 import eu.antidotedb.client.NoTransaction;
 import eu.antidotedb.client.RegisterRef;
 import eu.antidotedb.client.ValueCoder;
@@ -20,6 +21,16 @@ public class NoTxTest extends AbstractAntidoteTest {
         assertEquals("Abc", reg.read(tx));
         reg.set(tx, "xyz");
         assertEquals("xyz", reg.read(tx));
+    }
+
+    @Test
+    public void counterNeg() {
+        CounterRef reg = bucket.counter("blubber");
+        NoTransaction tx = antidoteClient.noTransaction();
+        reg.increment(tx, 1);
+        assertEquals(1, (int) reg.read(tx));
+        reg.increment(tx, -2);
+        assertEquals(-1, (int) reg.read(tx));
     }
 
     @Test
